cfa90358c9ad2e7635d089233d46341825bf5e3c
Andreas Wagner <whysthatso@gmx.de>
2016-03-13 22:02:42 +0200

    fix styles, remove unnecessary widget area, fix search bar

--- functions.php
+++ functions.php
@@ -195,19 +195,8 @@ if (function_exists('register_sidebar'))
   // Define Sidebar Widget Area 1
   register_sidebar(array(
     'name' => __('Widget Area 1', 'html5blank'),
-    'description' => __('Description for this widget-area...', 'html5blank'),
+    'description' => __('Widgets in Seitenleiste', 'html5blank'),
     'id' => 'widget-area-1',
-    'before_widget' => '<div id="%1$s" class="%2$s">',
-    'after_widget' => '</div>',
-    'before_title' => '<h3>',
-    'after_title' => '</h3>'
-  ));
-
-  // Define Sidebar Widget Area 2
-  register_sidebar(array(
-    'name' => __('Widget Area 2', 'html5blank'),
-    'description' => __('Description for this widget-area...', 'html5blank'),
-    'id' => 'widget-area-2',
     'before_widget' => '<div id="%1$s" class="%2$s">',
     'after_widget' => '</div>',
     'before_title' => '<h3>',
--- sidebar.php
+++ sidebar.php
@@ -1,14 +1,10 @@
 <!-- sidebar -->
 <aside class="sidebar" role="complementary">
 
-  <?php get_template_part('searchform'); ?>
+  <div id="sidebar-search"> <?php get_template_part('searchform'); ?> </div>
 
   <div class="sidebar-widget">
     <?php if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('widget-area-1')) ?>
-  </div>
-
-  <div class="sidebar-widget">
-    <?php if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('widget-area-2')) ?>
   </div>
 
 </aside>

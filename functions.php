24f8a329a41fda8814ed109a6c7cb32c06ab84e0
root <root@vs1.whyservices.eu>
2016-03-13 23:19:37 +0100

    move styles file

--- functions.php
+++ functions.php
@@ -130,7 +130,7 @@ function html5blank_styles()
 {
   wp_register_style('normalize', get_template_directory_uri() . '/styles/normalize.css', array(), '1.0', 'all');
   wp_enqueue_style('normalize'); // Enqueue it!
-  wp_register_style('html5blank', get_template_directory_uri() . '/styles/style.css', array(), '1.0', 'all');
+  wp_register_style('html5blank', get_template_directory_uri() . '/style.css', array(), '1.0', 'all');
   wp_enqueue_style('html5blank'); // Enqueue it!
 }
 
